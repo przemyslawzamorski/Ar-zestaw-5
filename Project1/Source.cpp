48373aa24438bf12a56c5c24c2e7d5a4fab7de93
Zamor <przemyslaw.zamorskii@gmail.com>
2016-05-19 23:17:14 +0200

    dziala obliczanie silni

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -16,103 +16,85 @@ int main(int argc, char **argv) {
 
 	if (rank == 0){
 		int n, t, s;
+		MPI_Status status;
 		cin >> n >> t >> s;
-		
+
 		int* task_table = new int[n];
 		srand(s * time(NULL));
 
 		//petla rozsylajaca wykonuje sie n+size  jezeli i>n to wysylaj zalozmy zero lub tag 0 i przy odbiorze jezeli bedze to cos to koncz odbieranie :)
-		for (int i = 0; i < n ; i++){
-				task_table[i] = (rand() % t) + 1;
+		for (int i = 0; i < n; i++){
+			task_table[i] = (rand() % t) + 1;
 		}
 
-		int trigger = 0;
+
 		int counter = 1;
-		for (int i = 0; i < 2 * n; i++){
+		for (int i = 0; i < n; i++){
 
-			if (i<n){
-				trigger = task_table[i];
+			if (i < size - 1){
+				MPI_Send(&task_table[i], 1, MPI_INT, i + 1, 1, MPI_COMM_WORLD);
+				cout << i + 1 << " " << task_table[i] << endl;
+			
 			}
 			else{
-				cout << endl;
-				trigger = 0;
-			}
+				/*cout << "0" << " " << endl;*/
 
-			counter = counter%size;
-			if (counter == size || counter ==0){
-				counter = 1;
+				/*to do odbiór*/
+				/*MPI_Send(&trigger, 1, MPI_INT, counter + 1, 1, MPI_COMM_WORLD);*/
 			}
-			cout << counter;
-			/*MPI_Send(&trigger, 1, MPI_INT, counter + 1, 1, MPI_COMM_WORLD);*/
-			counter++;
-			
-			
-			
-
 		}
-
-			/*if (i < size){
-				MPI_Send(&task_table[i], 1, MPI_INT,i+1, 1, MPI_COMM_WORLD);
-			}
-			else{
-				int licznik;
-				int* cos=new int [size];
-				/*zbieranie*/
-				/*MPI_Recv(&licznik, 1, MPI_INT, MPI_ANY_TAG, MPI_ANY_SOURCE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				cos[i - n] = licznik;
-
-
-
-				MPI_Send(0, 1, MPI_INT, i + 1, 1, MPI_COMM_WORLD);
-			}
-		}*/
 	}
 	else{
 
 		/*impelemntacja zmiennych pomocniczych*/
-	/*	int liczba = 0;
-	
+		int liczba = 0;
 
-		do
-		{
+
+		/*	do
+			{*/
+
 		MPI_Recv(&liczba, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		int r = 0;
-		int mianownik = 0;
-		int silnia = 1;
 
-		if (liczba == 0){
+		double r = 0;
+		double mianownik = 1;
+		double silnia = 1;
+
+		/*if (liczba == 0){
 			r = 0;
-			MPI_Send(&r, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-
-
-		}
+			/*MPI_Send(&r, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);*/
+		/*}
 		else{
 			if (liczba == 1){
-				
+
 				r = 1;
-				MPI_Send(&r, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+				/*MPI_Send(&r, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);*/
+				/*cout << "proces " << rank << " obliczyla " << r << endl;*/
 				/*dodaæ mierzenie czasu*/
 			/*}
-			else{
+			else{*/
 				for (int i = liczba; i > 1; i--){
-					silnia *= i;
-					mianownik += i;
+					silnia = silnia* i;
+					mianownik = mianownik+ i;
 				}
 
 				r = silnia / mianownik;
-			}
-			MPI_Send(&r, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+				cout << "proces " << rank << " silnia " << silnia << endl;
+				cout << "proces " << rank << " mianownik " << mianownik << endl;
+				cout << "proces " << rank << " wynik " << r << endl;
+			/*}
+			/*MPI_Send(&r, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);*/
 			/*dodaæ mierzenie czasu*/
-		/*}
+			/*}
 
-		} while (liczba == 0);*/
-	}
-
-
+			} while (liczba == 0);*/
+		}
 
 
-	
-	MPI_Finalize();
-	return 0;
 
+
+
+		MPI_Finalize();
+		return 0;
+
+	//}
 }
